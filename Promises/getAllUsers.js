9cb32f8e9e89a9f3867f869866b46c7718deb377
Arijit15091999 <arijitkarmakar056@gmail.com>
2024-02-18 02:00:01 +0530

    done

--- Promises/getAllUsers.js
+++ Promises/getAllUsers.js
@@ -1,7 +1,8 @@
-async function getAllUsers(url) {
+async function getAllDataInJson(url) {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+    return data;
 }
 
-getAllUsers("https://jsonplaceholder.typicode.com/users");
+getAllDataInJson("https://jsonplaceholder.typicode.com/users");
